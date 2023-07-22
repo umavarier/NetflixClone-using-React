ddcadb730c723976b0c1dadbd22122cc66e49910
Parvathi Kumar <parvathi.kumar1989@gmail.com>
2023-07-22 11:53:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
+import {originals, action} from './urls'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title='Netflic Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl= 'https://api.themoviedb.org/3'
+export const API_KEY='c5115f53608e456a406fa71872183e86'
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
